7dcec60b51eb891060b9d42a0169dfeb608fa617
cristobalaldezabal <cristobal.aldezabal@gmail.com>
2020-07-29 17:55:55 -0300

    dqf

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,27 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	var mesDelAño
+	mesDelAño=document.getElementById("txtIdMes").value;
 
+switch (mesDelAño)
+{
+	case "Junio": case "Julio": case "Agosto":
+{
+	alert("abrigate que hace frio");
+	break;
+}
+case "Enero": case "Febrero": case "Marzo": case "Abril": case "Mayo":
+{
+	alert("falta para el invierno");
+	break;
+}
+case "Septiembre": case "Octubre": case "Noviembre": case "Diciembre": 
+{
+alert("Ya paso el invierno ahora calor!!!");
+break;
+}
 
 
 
 }//FIN DE LA FUNCIÓN
+}
--- 8-TPs/05-jsAdivinaElNumero1.js
+++ 8-TPs/05-jsAdivinaElNumero1.js
@@ -7,20 +7,51 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
-var contadorIntentos;
-
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+  let numerosecreto;	
+	let maximo = 100;
+  let minimo = 1;
+  
+  numerosecreto = parseInt(Math.round(Math.random() * (maximo - minimo) + minimo));
 
 }
 
 function verificar()
-{
-	
-	
+{	
+  let numerosecreto;
+	let maximo =100;
+  let minimo =1;
+  
+
+  let contador = 1; // = 1 ? 
+  let adivinador;
+while(contador > 1)
+	{
+    contador = contador + 1;
+		document.getElementById("txtIdIntentos").value = contador;
+	}
+
+  // no se como generar el loop del contador y no se como definir la var numerosecreto una sola vez. while? 
+  
+
+
+  document.getElementById("txtIdIntentos").value = contador;
+  adivinador = document.getElementById("txtIdNumero").value;
+  numerosecreto = "comenzar()";
+
+
+  if (adivinador == numerosecreto)
+  {
+    alert ("Usted es un ganador!, y en solo " + contador + " intentos.");
+  }
+  else if (adivinador < numerosecreto)
+  {
+    alert("Te quedaste corto, amigue");
+  }
+  else if (adivinador > numerosecreto)
+  {
+    alert ("Te pasaste, maquina");
+  }
+
 }
